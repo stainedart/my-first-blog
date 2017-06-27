6381e9d5f971575f3c5da1801bfbeb9c5f3bfe9a
stainedart <stainedart@gmail.com>
2017-06-26 20:34:16 -0400

    Added the drafting and deleting capability

--- blog/views.py
+++ blog/views.py
@@ -21,9 +21,9 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
-        return redirect('post_detail', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
@@ -37,9 +37,26 @@ def post_edit(request, pk):
             post = form.save(commit=False)
             post.pk = pk
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = models.Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html',{'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(models.Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_delete(request, pk):
+    post = get_object_or_404(models.Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
